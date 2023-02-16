b8cf5022fdf388b5469a0080524da69281f2b2f5
berylben <berylben966@gmail.com>
2023-02-16 13:53:53 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - starting point
+ *
+ * Return: 1 return value
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",58);
+	return (1);
+}
