1bdcd3cf80517930c8e1d86f81a6cd88e19b0a5a
berylben <berylben966@gmail.com>
2023-02-16 12:21:46 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - starting point
+ *
+ * use printf
+ * Return: 0 return value
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art \n";
+	return (0);
+}
