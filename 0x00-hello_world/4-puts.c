c8592e01946fa843d93e5fc826f8c3c2f074195f
berylben <berylben966@gmail.com>
2023-02-16 11:31:01 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
+/*
+ * main-starting point
+ * a program that uses the puts function
+ */
 
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle,\n");
-	return(0);
+	return (0);
 }
