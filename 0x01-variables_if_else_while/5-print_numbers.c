2e0d706b2780c633126fe717ecfc88cf00ede25a
berylben <berylben966@gmail.com>
2023-02-19 11:16:54 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * main - starting point
+ *
+ * Return: 0  success
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
