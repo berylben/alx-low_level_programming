1afad616fdeb600b6599d7013fbe8281a254e82d
berylben <berylben966@gmail.com>
2023-02-19 12:43:44 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+/**
+ * main - starting point
+ *
+ * Return: 0 return value
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 48; y > 58; y++)
+		{
+			if (y > x)
+			{
+				for (z = 48; z < 58; z++)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == 55 && y == 56 && z == 57)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
