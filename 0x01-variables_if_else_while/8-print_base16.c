955ee0dfb9628d81f96dcda9184fe9dc456fc1c7
berylben <berylben966@gmail.com>
2023-02-19 12:03:23 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
