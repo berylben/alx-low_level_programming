2afc4b5cff32607f291586a7e459f1c0e8a445ab
berylben <berylben966@gmail.com>
2023-02-19 08:17:14 -0800

    upperlower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,7 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		putchar(ch);
+		putchar("\n", ch);
 		ch++;
 	}
 
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+/**
+ * main - starting point
+ *
+ * Return: 0 (success)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
