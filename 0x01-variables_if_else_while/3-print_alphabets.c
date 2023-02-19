49114042e214127131c5ba8fd6cb0d260fc04748
berylben <berylben966@gmail.com>
2023-02-19 09:46:42 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,14 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 		putchar(ch);
-		putchar('\n');	
+	}
+	putchar('\n');	
 	
 	return (0);
 }
