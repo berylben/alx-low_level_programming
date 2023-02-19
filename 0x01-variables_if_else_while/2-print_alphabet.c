60fb6ae5654b03afc3a5da4c7566d127e2c1320a
berylben <berylben966@gmail.com>
2023-02-19 07:17:32 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * main - starting point
+ *
+ * Return: 0 return value
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	return (0);
+}
