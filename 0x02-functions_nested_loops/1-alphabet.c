44710154492b45e667cd9c54ff5529a0fb1e487c
berylben <berylben966@gmail.com>
2023-02-21 08:36:30 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
 
 #include "main.h"
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 #include <stdio.h>
-#include <unstd.h>
