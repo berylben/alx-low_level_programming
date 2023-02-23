05a505518deff77ae06d0dbc88f16264c4e57817
berylben <berylben966@gmail.com>
2023-02-22 19:01:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-	char *c = "_putchar";
-
-	while (*c != '\0')
-	{
-		_putchar(*c);
-	}
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,7 @@
-int _putchar(char c);
+#ifndef MAIN_H_
+#define GREET_H_
+
+int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_x10(void);
-int _isalpha(int c);
-int _islower(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-#include <stdio.h>
-_putchar
+
+#endif
