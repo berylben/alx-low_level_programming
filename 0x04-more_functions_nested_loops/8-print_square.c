53cb42c6381f26d71bf9c30ad75176cd33f5dd70
berylben <berylben966@gmail.com>
2023-02-23 13:27:10 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+/**
+ * print_square - prints a sqaure
+ * @size: size of the square
+ * Return: 0 success
+ */
+
+#include "main.h"
+
+void print_square(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
