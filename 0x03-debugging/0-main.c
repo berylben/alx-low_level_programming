996edb6f358f4b784cd59d4f7c05fb686df24398
berylben <berylben966@gmail.com>
2023-02-24 05:03:30 -0800

    positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+/**
+ * main - tests function that prnts if integer is positive or negative
+ * Return: o
+ */
+
+#include "main.h"
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
