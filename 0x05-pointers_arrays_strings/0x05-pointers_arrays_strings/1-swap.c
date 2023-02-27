b0d21b4aaa6b971a8396b7ec648c0ecd9e291e0d
berylben <berylben966@gmail.com>
2023-02-27 06:45:10 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer input
+ * @b: integer input
+ * Return: Always 0 (success)
+ */
+
+#include "main.h"
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
--- 0x05-pointers_arrays_strings/0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H_
 #define GREET_H_
-
-void reset_to_98(int *n);
+int _putchar(char);
 void swap_int(int *a, int *b);
-
 #endif
