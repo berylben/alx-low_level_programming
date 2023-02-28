c8a799426d42d06f1bd81f59035c8c9500974270
berylben <berylben966@gmail.com>
2023-02-28 08:45:23 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+/**
+ * _atoi - converts a string to an integer
+ * @s: string input
+ * Return: 0 always
+ */
+
+#include "main.h"
+
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, ai = 0, pq= 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pq *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		ai = ai + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (ai * pq);
+}
