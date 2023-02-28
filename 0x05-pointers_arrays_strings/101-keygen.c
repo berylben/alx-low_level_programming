e1cf780023545fa59521368449fd07ab221f0bcf
berylben <berylben966@gmail.com>
2023-02-28 10:57:23 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+/**
+ * main - generate random valid password
+ *
+ * Return: 0 always
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int man(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
