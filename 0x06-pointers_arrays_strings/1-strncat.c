53bb77e87f6f88a7738cc76a674dd4d76623430a
berylben <berylben966@gmail.com>
2023-03-02 06:27:48 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ * _strncat - copies a string of a number
+ * @dest: stores a string copy
+ * @src: source of a string
+ * @n: number of bytes from the source
+ * Return: pointer to resulting string dest
+ */
+
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H_
 #define GREET_H_
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #endif
