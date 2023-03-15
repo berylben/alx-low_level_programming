ab3e9df110da1787596453f90a29d13aa344d861
berylben <berylben966@gmail.com>
2023-03-15 05:46:56 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+/**
+ * main - entry point
+ * @argc: argument input
+ * @argv: array pointing to argument
+ * Return: Always (0) success
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%d\n", i - 1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H_
 #define GREET_H_
 int main(int argc, char *argv[]);
+int main(int argc, __attribute__((unused)) char *argv[]);
 #endif
