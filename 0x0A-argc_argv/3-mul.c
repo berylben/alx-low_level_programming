faaa88c8d1dd497d527b849405b18113da247122
berylben <berylben966@gmail.com>
2023-03-15 06:52:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ * @argc: argument input
+ * @argv: array pointing to arguments
+ * Return: Always (0) success
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
