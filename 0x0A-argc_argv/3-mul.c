7ba32acf7463ca4dfc26e98c763970cc30241b39
berylben <berylben966@gmail.com>
2023-03-15 06:58:04 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,15 +7,24 @@
 
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i, res = 1;
 
-	while (i < argc)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
-		i++;
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
 	}
 	return (0);
 }
