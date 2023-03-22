e7b0470420ae59b33fdf1bd33416262d5dfae09b
berylben <berylben966@gmail.com>
2023-03-22 11:02:07 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,8 +5,8 @@
  * Return: 0 success
  */
 
-#include "main.h"
-
+#include "function_pointers.h"
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H_
+#define GREET_H_
+void print_name(char *name, void (*f)(char *));
+#endif
