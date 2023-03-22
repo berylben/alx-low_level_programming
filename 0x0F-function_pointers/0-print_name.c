6f7192aa595172e7006dc33bb66bfa45413f5909
berylben <berylben966@gmail.com>
2023-03-22 10:46:35 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function to use to print name
+ * Return: 0 success
+ */
+
+#include "main.h"
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
