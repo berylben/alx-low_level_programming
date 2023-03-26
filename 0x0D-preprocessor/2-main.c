c6ff69112f2709ca8c2fddedd2b86971664ff2f7
berylben <berylben966@gmail.com>
2023-03-25 20:46:08 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Always (0) success
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
