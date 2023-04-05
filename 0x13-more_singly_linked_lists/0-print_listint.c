94edeb57aac90987110140ea44fc82c0b2cb5609
berylben <berylben966@gmail.com>
2023-04-05 12:06:17 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+/**
+ * print_listint - prints all nodes data
+ * @h: the haed noe pointer
+ * Return: the numebrof nodes in list
+ */
+
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+More singly linked lists
