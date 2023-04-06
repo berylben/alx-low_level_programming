705e45df646aba875c44c02e6eccda562794bc9a
berylben <berylben966@gmail.com>
2023-04-06 06:50:50 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+	temp = head->next;
+	free(head);
+	head = temp;
+}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to the list to be freed
+ */
+
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
